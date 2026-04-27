d1297d84ae4d27112ab91722babd2badb42d0ff9
Dannggeorrgge <dannggeorrgge@gmail.com>
2026-04-27 05:54:20 +0100

    Updated header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -24,7 +24,7 @@ export function Header({ onContactClick }: { onContactClick: () => void }) {
           animate={{ opacity: 1 }}
           className="font-semibold text-xl tracking-tighter text-primary"
         >
-          QU33K LIMITED
+          QU33K
         </motion.div>
 
         {/* Desktop Navigation */}
